004be002106579cf0f826c96dc52bffd4d91b86f
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-09 15:44:48 +0530

    nqmin.py

---
+++ nqmin.py
@@ -0,0 +1,16 @@
+n=int(input("enter n"))
+q=int(input("enter q"))
+l=[]
+l1=[]
+for i in range(n):
+    a=int(input("enter n val"))
+    l.append(a)
+for j in range(q):
+    c=0
+    u=int(input())
+    v=int(input())
+    for i in range(u,v):
+        l1.append(l)
+    print(min(l))
+        
+    
