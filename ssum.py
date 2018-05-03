f753f20df9b00ac08c3d52bf8c305f4c8447db8d
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-05-03 15:51:44 +0530

    ssum.py

---
+++ ssum.py
@@ -0,0 +1,18 @@
+n=int(input())
+k=int(input())
+l=[]
+c=0
+d=0
+b=1
+for i in range(n):
+    a=int(input())
+    l.append(a)
+l.sort()
+for i in range(n):
+    c=c+l[i]
+    
+    if b==0:
+        d=d+1
+    if c==k:
+        b=0
+print(d)
